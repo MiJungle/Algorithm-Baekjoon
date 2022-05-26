4a8b9fa824e5eeeba89a60297b2ffaba1c9491f7
MiJung Lee <mijunglee@MiJungs-MacBook-Air.local>
2022-05-26 15:29:33 +0900

    20220526

--- week08/78_subsets.py
+++ week08/78_subsets.py
@@ -6,10 +6,29 @@ class Solution(object):
     def subsets(self, nums):
         ans = []
         def dfs(index, path):
-            
             ans.append(path)
-
             for i in range(index, len(nums)):
-                dfs(i+1, path + [nums[i]])
+                dfs(i+1, path+ [nums[i]])
         dfs(0,[])
         return ans
+print(Solution().subsets(nums))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
