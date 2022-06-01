6f0da5a25816faeebbc4d1a9859aa4542fb078fb
MiJung Lee <mijunglee@MiJungs-MacBook-Air.local>
2022-06-01 11:01:53 +0900

    20220601

--- week09/b_2805.py
+++ week09/b_2805.py
@@ -2,29 +2,44 @@ import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-trees = list(map(int, input().split()))
-trees.sort()
-ans = []
+n,m = map(int,input().split())
+tree = list(map(int, input().split()))
+tree.sort()
 
-def cut_tree():
-    left = 0
-    right = n-1
+left = 1
+right = max(tree)
+while left <= right:
     mid = (left+right)//2
-    length = trees[mid]
+    ans = 0
+    for t in tree:
+        if t > mid:
+            ans += t-mid
+    if ans >= m:
+        left = mid+1
+    else:
+        right = mid-1
+
+print(right)
+
+
+
+
+
+
 
  #최대 길이는 마지막 나무 높이만큼 
-    num = 0
-    idx = 0
-    while num != m:
-        if trees[idx] > length:
-            num += trees[idx]- length
-        if num == m: #m 값 목표 도달 
-            return length
-        elif num > m:
-            length = length + 1
-        else: 
-            length = length - 1
-        idx+= 1
-        return length
-print(cut_tree())
+#     num = 0
+#     idx = 0
+#     while num != m and idx != len(trees)-1:
+#         if trees[idx] > length:
+#             num += trees[idx]- length
+#             idx+= 1
+#             print(idx)
+#         return length
+#     if num > m:
+#         length = length + 1
+#     else: 
+#         length = length - 1
+
+#     return length
+# print(cut_tree())
