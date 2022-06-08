76179eec7577f1a501881a98a121a9ba3a8df54f
MiJung Lee <mijunglee@MiJungs-MacBook-Air.local>
2022-06-08 12:26:54 +0900

    20220608

---
+++ week10/b_2630.py
@@ -0,0 +1,28 @@
+import sys
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+n = int(input())
+paper = [list(map(int,input().split())) for _ in range(n)]
+
+blue = 0
+white = 0
+
+def papercut(x,y,n):
+    global blue, white
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if paper[x][y] != paper[i][j]:
+                papercut(x,y,n//2)
+                papercut(x,y+n//2,n//2)
+                papercut(x+n//2,y,n//2)
+                papercut(x+n//2,y+n//2,n//2)
+                return
+    if paper[x][y]==0:
+        white+=1
+    else:
+        blue+=1
+    
+papercut(0,0,n)
+print(white)
+print(blue)
