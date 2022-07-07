417e3c981994928e144f62872ed15853736a163c
MiJung Lee <mijunglee@MiJungs-MacBook-Air.local>
2022-07-07 18:19:26 +0900

    20220707

---
+++ week14/424_longest_repeating.py
@@ -0,0 +1,23 @@
+s = "ABAB"
+k = 2
+Output: 4
+
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        d= {}
+        i = 0
+        ans = 0
+        for j in range(len(s)):
+            d[s[j]]= d.get(s[j],0) +1
+            cnt = j-i+1 - max(d.values())
+            while cnt > k:
+                d[s[i]]-= 1
+                i+= 1
+                cnt = j-i+1 -max(d.values())
+            ans = max(j-i+1, ans)
+        return ans
