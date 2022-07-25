ad488b448f1b3929f00dc5c10c5e62aebd2b6152
MiJung Lee <mijunglee@MiJungs-MacBook-Air.local>
2022-07-25 13:45:35 +0900

    20220725

---
+++ week15/p_64065.py
@@ -0,0 +1,21 @@
+s = "{{20,111},{111}}"
+
+def solution(s):
+    for i in s:
+        
+    # arr = sorted(s, key = lambda x: len(x) )
+    # arr = []
+
+    # print(arr)
+    # answer = []
+    # for i in arr:
+    #     for j in i:
+    #         if j.isnumeric():
+    #                 answer.append(int(j))
+    # final = []
+    # for i in answer:
+    #     if i not in final:
+    #         final.append(i)
+    # return final
+
+print(solution(s))
