153c58e2085d7eb58f0b5449ad70bddaa9a257bd
MiJung Lee <mijunglee@MiJungs-MacBook-Air.local>
2022-08-02 00:00:24 +0900

    20220802

---
+++ week15/p_43238.py
@@ -0,0 +1,16 @@
+# 시간초과
+# 
+# n =6
+# times = [7, 10]	
+
+
+# def solution(n, times):
+#     arr = []
+#     for i in range(1,n):
+#         for time in times:
+#             arr.append(time*i)
+#     arr.sort()
+#     return arr[n-1]
+
+
+# print(solution(n,times))
