c43e8062c8bdf178c10b7153730a4d6aebce11ad
MiJung Lee <mijunglee@MiJungs-MacBook-Air.local>
2022-10-01 23:22:20 +0900

    20221001

--- week22/d.js
+++ week22/d.js
@@ -1,6 +1,36 @@
-console.log(0.1 + 0.2 === 0.3)
-console.log(1 + 2 === 3)
-console.log(1>2 && 3> 2 === true)
-    console.log(false == undefined)
-        console.log(false == null)
-            console.log(null == undefined)
+let a = {
+    "id": "1",
+    "title": "Back-end 협업 선호도 조사",
+    "description": "Back-end가 어떤 기술로 구성하는 것을 선호하십니까?",
+    "steps": [
+      [
+        {
+          "type": "text",
+          "label": "당신의 이름은?",
+          "validation": {
+            "isRequired": true
+          }
+        }
+      ],
+      [
+        {
+          "name": "prefer",
+          "type": "singleChoice",
+          "label": "다음중 가장 선호하는 Back-end 연동 방식은?",
+          "options": [
+            "RESTful API",
+            "GraphQL",
+            "RPC",
+            "그외"
+          ]
+        }
+      ]
+    ]
+  }
+  
+  for(let i =0 ; i <a.steps.length;i++){
+    let k = a.steps[i][0].label
+    console.log(k)
+  }
+
+  console.log(a.steps.length)
---
+++ week22/k.js
@@ -0,0 +1,10 @@
+const array = [{a:10} , {b:2}, {c:3} ] 
+
+function addToArray(array,element){
+    const newArray = [...array]
+    newArray.push(element)
+    newArray[0].a = 10;
+    console.log(newArray);
+}
+addToArray(array, {d:4})
+console.log(array)
