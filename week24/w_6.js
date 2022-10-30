4514743b5ef30db4a4b2fce3c26f0c862039459e
MiJung Lee <mijunglee@MiJungs-MacBook-Air.local>
2022-10-30 23:39:13 +0900

    20221030

--- week24/w_6.js
+++ week24/w_6.js
@@ -1,27 +1,41 @@
-describe("problem6", () => {
-    test("case1", () => {
-      expect(
-        problem6([
-          ["jm@email.com", "제이엠"],
-          ["jason@email.com", "제이슨"],
-          ["woniee@email.com", "워니"],
-          ["mj@email.com", "엠제이"],
-          ["nowm@email.com", "이제엠"],
-        ])
-      ).toEqual(["jason@email.com", "jm@email.com", "mj@email.com"]);
-    });
-  });
+// describe("problem6", () => {
+//     test("case1", () => {
+//       expect(
+//         problem6([
+//           ["jm@email.com", "제이엠"],
+//           ["jason@email.com", "제이슨"],
+//           ["woniee@email.com", "워니"],
+//           ["mj@email.com", "엠제이"],
+//           ["nowm@email.com", "이제엠"],
+//         ])
+//       ).toEqual(["jason@email.com", "jm@email.com", "mj@email.com"]);
+//     });
+//   });
   
 
 
 function problem6(forms) {
-    var answer;
+    var answer= []
+
+    forms.forEach((form1)=> {
+        for(let i =0 ; i < form1[1].length-1; i++){
+        let two_letter = form1[1][i] + form1[1][i+1]
+        forms.forEach((form2)=> {
+            if(form2[1].includes(two_letter)&& forms.indexOf(form1)!= forms.indexOf(form2)){
+                if(!answer.includes(form1[0])) answer.push(form1[0])
+            }
+        })
+    }
+
+    })
+
+    answer.sort((a, b) => a.localeCompare(b));
     return answer;
   }
   
 
-  forms = [ ["jm@email.com", "제이엠"], 
-        ["jason@email.com", "제이슨"], 
+  forms = [ ["Bm@email.com", "제이엠"], 
+        ["ason@email.com", "제이슨"], 
         ["woniee@email.com", "워니"], 
         ["mj@email.com", "엠제이"], 
         ["nowm@email.com", "이제엠"] ]
